500aad65f2fdf1aa01d29700b0c4dae6ce0e1454
gbaranski <gbaranski@gbaranski-ubuntu.lan>
2020-03-06 21:12:27 +0100

    no longer search for promoted offers added database printing description of offer

--- main.py
+++ main.py
@@ -16,11 +16,9 @@ messageString = "Dzien dobry, \n Czy bylby Pan zainteresowany wymiana na kompute
 def sendMessage(messageUrl):
     driver.get(messageUrl)
     print("Message is sending!")
-    messageTextAreaPath = "//*[@id=\"ask-text\"]"
-    messageTextArea = driver.find_element_by_xpath(messageTextAreaPath)
+    messageTextArea = driver.find_element_by_xpath("//*[@id=\"ask-text\"]")
     messageTextArea.send_keys(messageString)
-    submitButtonPath = "//*[@id=\"contact-form\"]/fieldset/div[3]/div/span/input"
-    submitButton = driver.find_element_by_xpath(submitButtonPath)
+    submitButton = driver.find_element_by_xpath("//*[@id=\"contact-form\"]/fieldset/div[3]/div/span/input")
     submitButton.click()
     time.sleep(3)
     getListOfOffers("https://www.olx.pl/pomorskie/q-macbook/")
@@ -34,54 +32,80 @@ def openOfferPage(offerUrl):
     
 def getNextPageUrl():
     print("Changing to next page")
-    nextPageButtonPath = "//*[@id=\"body-container\"]/div[3]/div/div[8]/span[9]/a"
     try:
-        nextButtonURL = driver.find_element_by_xpath(nextPageButtonPath).get_attribute('href')
+        nextButtonURL = driver.find_element_by_xpath("//*[@id=\"body-container\"]/div[3]/div/div[8]/span[9]/a").get_attribute('href')
         print(nextButtonURL)
         getListOfOffers(nextButtonURL)
     except NoSuchElementException as exception:
         print("Its last page!")
 
+def additionalOfferInfo(offerUrl):
+    print("Printing description of a offer")
+    additionalBrowser = Chrome()
+    additionalBrowser.get(offerUrl)
+    offerDescription = additionalBrowser.find_element_by_xpath("//*[@id=\"textContent\"]").text
+    additionalBrowser.close()
+    return offerDescription
+
+
 def askUserDoesHeWant(offerUrl):
-    print("If you want it, press Y, else N")
+    print("If you want it, press Y, else N. For offer description I. For link L")
     userKey = readchar.readkey()
     if (userKey == "y" or userKey == 'Y'):
         print("yes")
+        offerDatabase = open("offerDatabase.txt", "a")
+        offerDatabase.write(offerUrl)
     elif (userKey == 'n' or userKey == 'N'):
         print("no")
+    elif (userKey == 'i' or userKey == 'I'):
+        print("------------------------------")
+        print(additionalOfferInfo(offerUrl))
+        askUserDoesHeWant(offerUrl)
+    elif (userKey == 'l' or userKey == 'L'):
+        print(offerUrl)
     elif (userKey == readchar.key.CTRL_C):
         print("Bye")
+        driver.exit()
         exit()
     else:
         askUserDoesHeWant(offerUrl)    
 
+def checkIfFileContainsString(stringToSearch):
+    offerDatabase = open("offerDatabase.txt", "r")
+    for line in offerDatabase:
+        if (stringToSearch in line):
+            return True
+            offerDatabase.close()
+ 
 def getListOfOffers(offerListUrl):
+    
     print("Getting list of offers")
+    
     driver.get(offerListUrl)
-    arrayOfferNames = driver.find_elements_by_xpath("//a[contains(@class, 'marginright5') and contains(@class, 'link') and contains(@class, 'linkWithHash') and contains(@class, 'detailsLink')]")
+    arrayOfferNames = driver.find_elements_by_xpath("//a[contains(@class, 'marginright5') and contains(@class, 'link') and contains(@class, 'linkWithHash') and contains(@class, 'detailsLink') and not(contains(@class, 'detailsLinkPromoted'))]")
     arrayOfferPrice = driver.find_elements_by_xpath("//p[contains(@class, 'price')]")
     for offerName, offerPrice in zip(arrayOfferNames, arrayOfferPrice):
-        print(offerName.text)
-        print(offerPrice.text)
-        askUserDoesHeWant(offerName.get_attribute("href"))
+        print("------------------------------")
+        if (checkIfFileContainsString(offerName.get_attribute('href'))):
+            print("One offer has been skipped!")
+        else:
+            print(offerName.text)
+            print(offerPrice.text)
+            askUserDoesHeWant(offerName.get_attribute("href"))
     getNextPageUrl()
 
 
 def doAuth(loginUrl):
     driver.get(loginUrl)
-    mojOlxButtonPath = "//*[@id=\"topLoginLink\"]"
-    mojOlxButton = driver.find_element_by_xpath(mojOlxButtonPath)
+    mojOlxButton = driver.find_element_by_xpath("//*[@id=\"topLoginLink\"]")
     mojOlxButton.click()
     time.sleep(3)
     print("Logging using predefined password and login")
-    loginFieldPath = "//*[@id=\"userEmail\"]"
-    passwordFieldPath = "//*[@id=\"userPass\"]"
-    loginField = driver.find_element_by_xpath(loginFieldPath)
-    passworldField = driver.find_element_by_xpath(passwordFieldPath)
+    loginField = driver.find_element_by_xpath("//*[@id=\"userEmail\"]")
+    passworldField = driver.find_element_by_xpath("//*[@id=\"userPass\"]")
     loginField.send_keys(login)
     passworldField.send_keys(password)
-    buttonPath = "//*[@id=\"se_userLogin\"]"
-    loginButton = driver.find_element_by_xpath(buttonPath)
+    loginButton = driver.find_element_by_xpath("//*[@id=\"se_userLogin\"]")
     time.sleep(2) # safe logging in
     loginButton.click()
     time.sleep(2) # safe logging in
